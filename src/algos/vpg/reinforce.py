259ae9037ee0b693422a120716f71dde9ff9183d
Salah Chadli <salah.chadli@gmail.com>
2020-11-06 20:05:18 +0100

    define Agent for reinforce

--- src/algos/vpg/reinforce.py
+++ src/algos/vpg/reinforce.py
@@ -19,3 +19,52 @@ class Policy(T.nn.Module):
         x = T.nn.functional.tanh(self.l2(x))
         x = self.l3(x)
         return x
+
+
+class Agent():
+    def __init__(self, lr, input_dims, gamma=0.99, n_actions=4):
+        self.gamma = gamma
+        self.lr = lr
+        self.reward_memory = []
+        self.action_memory = []
+        self.policy = Policy(self.lr, input_dims, n_actions)
+
+    def choose_action(self, observation):
+        observation = T.Tensor(observation)
+        state = T.autograd.Variable(observation)
+        probabilities = T.nn.functional.softmax(self.policy.forward(state))
+        action_probs = T.distributions.Categorical(probabilities)
+        action = action_probs.sample()
+        log_probs = action_probs.log_prob(action)
+        self.action_memory.append(log_probs)
+        
+        return action.item()
+
+
+    def store_rewards(self, reward):
+        self.reward_memory.append(reward)
+    
+    def learn(self):
+        self.policy.optimizer.zero_grad()
+        G = np.zeros_like(self.reward_memory)
+        for t in range(len(self.reward_memory)):
+            G_sum = 0
+            discount = 1
+            for k in range(t, len(self.reward_memory)):
+                G_sum += self.reward_memory[k] * discount
+                discount *= self.gamma
+            G[t] = G_sum
+        G = T.tensor(G)
+        
+        #loss = - G[0] * np.sum(self.action_memory)
+        loss = 0
+        for g, logprob in zip(G, self.action_memory):
+            loss += -g * logprob
+        loss.backward()
+        self.policy.optimizer.step()
+
+        steps_nb = len(self.reward_memory)
+        self.action_memory = []
+        self.reward_memory = []
+
+        return loss.item(), steps_nb
